cd7d8c0c8bff7045393c2b7a517f280d88216556
Shaira_Lorraine Montojo <smontojo_200000000067@uic.edu.ph>
2024-04-08 04:46:29 +0800

    comments and dummy account addition

--- Login Form/login/login.js
+++ Login Form/login/login.js
@@ -1,15 +1,15 @@
 function login(){
 
-    const usName = document.getElementById("useName").value;
-    const pass = document.getElementById("passWord").value;
+    const usName = document.getElementById("useName").value; //getting the value of the username input box
+    const pass = document.getElementById("passWord").value; //getting the value of the password inout box
 
-    const dummuser = usName;
-    const dummpass = pass;
+    const dummuser = "admin";
+    const dummpass = "123";
 
 //Condition for account authentication
 if(usName==dummuser && pass==dummpass){
 
-    window.location.assign("homepage.html");
+    window.location.assign("homepage.html"); //if the account is correct, the page from login page will redirect to the homepage
     
 
 }
