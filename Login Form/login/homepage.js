a3b7c650035b3faa8f328d40271662fb8cde852f
Shaira_Lorraine Montojo <smontojo_200000000067@uic.edu.ph>
2024-04-08 20:56:01 +0800

    pop up box for confirmation is added

--- Login Form/login/homepage.js
+++ Login Form/login/homepage.js
@@ -1,4 +1,13 @@
 function login(){
 
-    window.location.assign("login.html");
+    let text;
+
+    if(confirm("Are you sure?")==true){ //pop up box for confirmation to logout will appear
+    
+        window.location.assign("login.html"); //if the condition is met, the page will redirect to the login page.
+
+    }
+    else{
+        return;
+    }
 }
